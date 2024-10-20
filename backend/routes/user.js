ca29d6de042180244263858cf5dffa8e83154902
Your aryan <aryan9090900@gmail.com>
2024-10-21 00:10:32 +0530

    Initial commit

---
+++ backend/routes/user.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const zod = require("zod");
+const { User, Account } = require("../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { authMiddleware } = require("../middleware");
+
+const router = express.Router();
+
+// USER SIGN UP
+
+const signupBody = zod.object({
+  username: zod.string().email(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
+
+router.post("/signup", async (req, res) => {
+  const { success } = signupBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+    
+
+  const existingUser = await User.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already taken",
+    });
+  }
+
+  const { username, firstName, lastName, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hashSync(password, salt);
+  const newUser = await User.create({
+    username,
+    firstName,
+    lastName,
+    password: hashedPassword,
+  });
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    process.env.JWT_SECRET
+  );
+
+  res.status(200).json({
+    message: "User created successfully",
+    token: token,
+  });
+}});
+module.exports = router;
